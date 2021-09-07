1ae3a5bcfad6da5216060b7b4a81b74b0cac7af7
Rohan27s <74227860+Rohan27s@users.noreply.github.com>
2021-09-07 10:19:25 +0530

    Create 03. Collisions.cpp

---
+++ 03. Collisions.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*******  All Required define Pre-Processors and typedef Constants *******/
+#define ff first
+#define ss second
+#define mid(l, r) (l + (r - l) / 2)
+#define int long long
+#define mod 1000000007
+#define MOD1 = 998244353;
+#define pb push_back
+#define mp make_pair
+#define pq_max priority_queue<int>
+#define pq_min priority_queue<int, vi, greater<int>>
+#define INF 1e18
+#define IN(A, B, C) assert(B <= A && A <= C)
+#define loop(i, a, b) for (int i = (a); i < (b); i++)
+#define looprev(i, a, b) for (int i = (a); i >= (b); i--)
+#define ps(x, y) fixed << setprecision(y) << x
+#define w(x)  \
+    int x;    \
+    cin >> x; \
+    while (x--)
+#define sz(a) (int)(a.size())
+#define Y second
+#define eb emplace_back
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define print cout <<
+#define take(t) cin >> t;
+#define LSB(i) ((i) & -(i))
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<pii> vpi;
+typedef vector<vi> vvi;
+typedef map<int, int> mpii;
+typedef set<int> seti;
+typedef multiset<int> mseti;
+
+/****** Template of some basic operations *****/
+template <typename T, typename U>
+inline void amin(T &x, U y)
+{
+    if (y < x)
+        x = y;
+}
+template <typename T, typename U>
+inline void amax(T &x, U y)
+{
+    if (x < y)
+        x = y;
+}
+/**********************************************/
+
+/******** User-defined Function *******/
+void rohancpp()
+{
+    int n, m;
+    cin >> n >> m;
+    int ans = 0;
+    map<int, int> m1;
+    loop(i, 0, n)
+    {
+        int x, u;
+        cin >> x >> u;
+        m1[x * u]++;
+    }
+    loop(i, 0, m)
+    {
+        int y, v;
+        cin >> y >> v;
+        if (m1.count(y * v))
+        {
+            ans++;
+            m1[y * v]--;
+            if (m1[y * v] == 0)
+            {
+                m1.erase(y * v);
+            }
+        }
+    }
+    print ans << "\n";
+}
+
+int32_t main()
+{
+
+    w(x)
+    {
+        rohancpp();
+    }
+}
